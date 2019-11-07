18cffaadbb5e4027e2e29304d03bbd1d70554296
JiranKurian <jirankuria@gmail.com>
2019-11-08 04:13:01 +0530

    OTP Successful

--- ServerSide/AppCenterAPI/SharedServices/OTPGenerationService.cs
+++ ServerSide/AppCenterAPI/SharedServices/OTPGenerationService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace AppCenterAPI.SharedServices
 {
-    public class OTPService
+    public class OTPGenerationService
     {
         public int GenerateOTP()
         {
