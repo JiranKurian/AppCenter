ed82e31fe5cda58bc1b128e524d5946deb5983b2
Winston Shaji Jacob <winstonshajijacob@gmail.com>
2019-11-08 08:25:37 +0530

    hald done APi

--- ClientSide/Admin/js/adminSide.js
+++ ClientSide/Admin/js/adminSide.js
@@ -16,5 +16,35 @@ function feedbackload() {
 
 	var JsonArray = JSON.parse(xmlHttp.responseText);
 	console.log(JsonArray);
-	JsonArray.forEach((element) => {});
+	JsonArray.forEach((element) => {
+        document.getElementById('fillfeed').innerHTML =' <tr class="tr-shadow" >
+                                           
+        <td>user name</td>
+        <td>
+            <span class="block-email">user mail id</span>
+        </td>
+        <td >11/11/11</td>
+        
+        <td ><span class="status--process">Processed/Pending</span> <!-- class="denied" -->
+            </td> 
+        
+        <td>
+            <div class="table-data-feature">
+                <button class="item" data-toggle="tooltip" data-placement="top" title="View" onclick="loadfeedback(id)">
+                    <i class="fas fa-gavel"></i>
+                </button>
+                
+             
+            </div>
+        </td>
+    </tr>
+    <tr class="spacer"></tr>
+    <tr class="tr-shadow">
+        
+    </tr>'
+    });
+}
+function loadfeedback(id)
+{
+    
 }
