3cdb6281292ba9ddbca6c35e65dd24fcdad726bc
Winston Shaji Jacob <winstonshajijacob@gmail.com>
2019-11-08 10:28:59 +0530

    Update adminSide.js

--- ClientSide/Admin/js/adminSide.js
+++ ClientSide/Admin/js/adminSide.js
@@ -64,7 +64,7 @@ function messageget() {
 function feedbackreply() {
 	var usermail = localStorage.getItem('MessMail');
 	var usermessage = localStorage.getItem('MessRec');
-	var rplymsg = getElementById('replymsg').value;
+	var rplymsg = document.getElementById('replymsg').value;
 	var msgdat = localStorage.getItem('MessDate');
 	var msgcode = parseInt(localStorage.getItem('MessId'));
 
